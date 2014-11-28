b930626595242a4f55fa5b2eb79230ae47972be1
jamelablack <jamela.black@gmail.com>
2014-11-28 09:36:05 -0500

    added category and post category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController::Base
   def index
+
+  end
+
+  def show
+  	@post = Post.find(params[:id])
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+
+  # get '/posts', to: 'posts#index'
+  # get '/posts/:id', to: 'posts#show'
+
+  resources :posts, except: [:destroy] do
+  	resources :comments, only [:create]
+
 end
