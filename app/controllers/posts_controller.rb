1d9c78110490463ccc8ae9604cbae6240833d0c6
jamelablack <jamela.black@gmail.com>
2014-12-24 10:07:51 -0500

    debug params error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,45 @@
-class PostsController < ActionController::Base
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
   	@posts = Post.all
-
   end
 
-  def show
-  	@post = Post.find(params[:id])
-
-  end
+  def show; end
 
   def new
+    @post = Post.new(post_params)
 	end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first #TODO: change once we have user authentication
+
+    if @post.save(post_params)
+      flash[:notice] = "Your post has been saved!"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "Your post has been updated!"
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
-
+private
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end 
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end 
 
 end
