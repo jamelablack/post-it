ab29ad28e447045aedf093178641c852c58b9ed6
jamelablack <jamela.black@gmail.com>
2015-01-14 07:49:09 -0500

    added user/edit and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+	before_action :set_post, only: [:show, :edit, :update]
+	before_action :require_same_user, only: [:edit, :udpate]
+ 
 	def new
 		@user = User.new
 
@@ -17,14 +19,42 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
+	def edit
+	end
+
+
+	def update
+		if @user.update(user_params)
+			flash[:notice] = "Your user profile has been updated"
+			redirect_to user_path(@user)
+		else
+			render :edit
+		end
+
+	end
+
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :password)
 
 	end
 
+	def set_post
+		@user = User.find(params[:id])
+	end
 
-
+	def require_same_user
+		if current_user != @user
+			flash[:error] = "You are not allowed to complete that action!"
+			redirect_to root_path
+		else
+			render :edit
+		end
+	end
 end
 
 
