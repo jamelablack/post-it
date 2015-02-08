61b10e620b4f7a6b6c4f0bd5af9013361c002bf6
jamelablack <jamela.black@gmail.com>
2015-02-07 23:57:51 -0500

    DRY'd up with sluggable module for Users, Posts, Categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 	include Voteable
+	include Sluggable
+
 	belongs_to :creator, foreign_key: "user_id", class_name: "User"
 	has_many :comments
 	has_many :post_categories
@@ -9,38 +11,6 @@ class Post < ActiveRecord::Base
 	validates :description, presence: true
 	validates :url, presence: true, uniqueness: true
 
-	before_save :generate_slug!
+	sluggable_column :title
 	
-
-
-	def to_param
-		self.slug 
-	end
-
-	def generate_slug!
-		the_slug = to_slug(self.title)
-		post = Post.find_by slug: the_slug
-		count = 2
-		while post && post != self
-			the_slug = append_suffix(the_slug, count)
-			post = Post.find_by slug: the_slug
-			count += 1
-		end
-		self.slug = the_slug.downcase
-	end
-
-	def append_suffix(str, count)
-		if str.split('-').last.to_i != 0
-			return str.split('-').slice(0...-1).join('-') + "-" + count.to_s 
-		else
-			return str + "-" + count.to_s
-		end
-	end
-
-	def to_slug(title)
-		str = title.strip
-		str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-		str.gsub! /-+/, "-"
-		str.downcase
-	end
 end
--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,4 +1,3 @@
-#Using Concerns
 module Voteable
 	extend ActiveSupport::Concern
 
