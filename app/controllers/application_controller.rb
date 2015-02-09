fff1ba3175fe50b366991a247912e995ce5130dc
jamelablack <jamela.black@gmail.com>
2015-02-09 08:06:02 -0500

    added admin & moderator roles, restricting category permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,20 @@ class ApplicationController < ActionController::Base
 	end
 
 	def require_user
-		if !logged_in?
+		unless logged_in?
 			flash[:error] = "You must be logged in to do that"
 			redirect_to root_path
 		end
 	end
+	def require_admin
+		access_denied unless logged_in? and current_user.admin?
+	end
+
+	def access_denied
+		flash[:error] = "Sawry.. you can't do that!"
+		redirect_to root_path
+	end
+
 end
 
 
